d3a1116f4ad4baecf0409d72461ce77aa6ec8c59
cjeanm <jeanmichelcid@gmail.com>
2024-10-27 20:58:46 -0400

    more checks

--- src/utils/type-utils.ts
+++ src/utils/type-utils.ts
@@ -1,7 +1,9 @@
 import type { components } from "./../../generated/api/api.ts";
 
 export type GeneratedObject = components['schemas']['Color'];
+type ColorsType = GeneratedObject['color']; 
 
+// ex 1:
 export const Colors = {
     RED: 'RED',
     GREEN: 'GREEN',
@@ -11,6 +13,27 @@ export const Colors = {
 
 } as const;
 
-type ColorsType = GeneratedObject['color']; 
 type Assert<T extends U, U> = T extends U ? (U extends T ? true : never) : never;
-type _ = Assert<ColorsType, keyof typeof Colors | undefined>;
+let assert1: Assert<ColorsType, keyof typeof Colors | undefined>;
+
+// exV 2:
+type Col = "RED" | "GREEN" | "BLUE" | "YELLOW";
+type UnionToTuple<U> = 
+    U extends infer I 
+        ? [I, ...UnionToTuple<Exclude<U, I>>]
+        : [];
+
+        
+type ConstClassValToTuple<T> = 
+    T extends { [key: string]: infer V } 
+        ? V extends string 
+            ? V[] 
+            : [] 
+        : [];        
+type ColorTuple = UnionToTuple<Col>; 
+type Color2Tuple = UnionToTuple<ColorsType>; 
+type Colors3Tuple = ConstClassValToTuple<typeof Colors>;
+
+let assert2: Assert<Color2Tuple, ColorTuple>;
+let assert3: Assert<ColorTuple, Colors3Tuple>;
+
